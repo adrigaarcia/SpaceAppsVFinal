41c48f4e362ef0533db6a6a2e88620de93f67c25
AbelChT <abelcht@outlook.es>
2018-10-22 00:18:28 +0200

    delete leflet

--- front-end/src/app/map/map.component.ts
+++ front-end/src/app/map/map.component.ts
@@ -9,21 +9,11 @@ import {icon, latLng, marker, tileLayer} from 'leaflet';
     styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-    baseLayer = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-    // Marker for the top of Mt. Ranier
-    summit = marker([46.8523, -121.7603], {
-        icon: icon({
-            iconSize: [25, 41],
-            iconAnchor: [13, 41],
-            iconUrl: 'assets/marker-icon.png',
-            shadowUrl: 'assets/marker-shadow.png'
-        }),
-        title: 'Prueba'
-    });
+    public baseLayer = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
     // Layers control object with our two base layers and the three overlay layers
-    layersControl = {
+    public layersControl = {
         baseLayers: {
             'Street Maps': this.baseLayer
         },
@@ -44,48 +34,48 @@ export class MapComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.options.layers.push(this.summit);
-        this.layersControl.overlays['sdfsdf'] = this.summit;
+        // Obtiene periodicamente la información
+        this.dataSource.getData(1).then((data: Array<RocketLaunchInfo>) => {
+            this.launchRocketsData = data;
+            const nextLaunch: RocketLaunchInfo = data[0];
+            // mymap.setView(nextLaunch.coordinates, 13);
+            this.options['center'] = latLng(nextLaunch.coordinates);
+            data.forEach((element) => {
+                let texto = '<b>' + element.name + '</b><br />'
+                    + element.id + '</b><br />'
+                    + 'Hora: ' + element.date.toLocaleTimeString() + '<br />'
+                    + 'Fecha: ' + element.date.toLocaleDateString() + '<br />'
+                    + 'Cohete: ' + element.rocket;
 
-        /* // Initial configuration of Map
-         const mymap = L.map('mapid').setView([0, 0], 13);
+                if (element.url != null) {
+                    texto = texto + '<br />'
+                        + 'Url: ' + element.url;
+                }
 
-         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?' +
-             'access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-             maxZoom: 18,
-             id: 'mapbox.streets'
-         }).addTo(mymap);
+                const actualMarker = marker(element.coordinates, {
+                    icon: icon({
+                        iconSize: [25, 41],
+                        iconAnchor: [13, 41],
+                        iconUrl: 'assets/marker-icon.png',
+                        shadowUrl: 'assets/marker-shadow.png'
+                    }),
+                    title: element.name
+                });
 
-         // Obtiene periodicamente la información
-         this.dataSource.getData(1).then((data: Array<RocketLaunchInfo>) => {
-             this.launchRocketsData = data;
-             const nextLaunch: RocketLaunchInfo = data[0];
-             mymap.setView(nextLaunch.coordinates, 13);
-             data.forEach((element) => {
-                 let texto = '<b>' + element.name + '</b><br />'
-                     + element.id + '</b><br />'
-                     + 'Hora: ' + element.date.toLocaleTimeString() + '<br />'
-                     + 'Fecha: ' + element.date.toLocaleDateString() + '<br />'
-                     + 'Cohete: ' + element.rocket;
+                this.options.layers.push(actualMarker);
+                this.layersControl.overlays[element.id.toString()] = actualMarker;
 
-                 if (element.url != null) {
-                     texto = texto + '<br />'
-                         + 'Url: ' + element.url;
-                 }
-                 L.marker(element.coordinates).addTo(mymap)
-                     .bindPopup(texto);
-
-             });
-         });
+            });
+        });
 
 
-         this.dataSource.onSelectedLaunchChangeListener((id: Number) => {
-             console.log(id);
-             const selected = this.launchRocketsData.find((element: RocketLaunchInfo) => {
-                 return element.id === id;
-             });
+        this.dataSource.onSelectedLaunchChangeListener((id: Number) => {
+            const selected = this.launchRocketsData.find((element: RocketLaunchInfo) => {
+                return element.id === id;
+            });
 
-             mymap.setView(selected.coordinates, 13);
-         });*/
+            // mymap.setView(selected.coordinates, 13);
+            this.options['center'] = latLng(selected.coordinates);
+        });
     }
 }
