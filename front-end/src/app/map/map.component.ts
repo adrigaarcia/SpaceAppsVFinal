53e7e9c3b4725b32505ea8419e80681251e33412
AbelChT <abelcht@outlook.es>
2018-10-22 19:21:43 +0200

    Usando solo componentes de angular

--- front-end/src/app/map/map.component.ts
+++ front-end/src/app/map/map.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SharedDataService} from '../shared-data.service';
 import {RocketLaunchInfo} from '../rocket-launch/rocket-launch';
-import {icon, latLng, marker, tileLayer} from 'leaflet';
+import {icon, latLng, marker, tileLayer, Layer, LatLng} from 'leaflet';
 
 @Component({
     selector: 'app-map',
@@ -10,72 +10,59 @@ import {icon, latLng, marker, tileLayer} from 'leaflet';
 })
 export class MapComponent implements OnInit {
 
+    // Base layer (MAP)
     public baseLayer = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-    // Layers control object with our two base layers and the three overlay layers
-    public layersControl = {
-        baseLayers: {
-            'Street Maps': this.baseLayer
-        },
-        overlays: {}
-    };
+    // Different launch markers
+    public markers: Layer[] = [];
 
+    // Center in the map
+    public center: LatLng = latLng([46.8523, -121.7603]);
+
+    // Initial options configuration
     public options = {
         layers: [
             this.baseLayer
         ],
-        zoom: 7,
+        zoom: 10,
         center: latLng([46.8523, -121.7603])
     };
 
     public launchRocketsData: Array<RocketLaunchInfo> = [];
 
+    private addMarker(title: string, position: [number, number]) {
+        const newMarker = marker(
+            position,
+            {
+                icon: icon({
+                    iconSize: [25, 41],
+                    iconAnchor: [13, 41],
+                    iconUrl: 'assets/marker-icon.png',
+                    shadowUrl: 'assets/marker-shadow.png'
+                }),
+                title: title
+            }
+        );
+
+        this.markers.push(newMarker);
+    }
+
     constructor(private dataSource: SharedDataService) {
     }
 
     ngOnInit() {
-        // Obtiene periodicamente la información
+        // Obtiene la información sobre lanzamientos
         this.dataSource.getData(1).then((data: Array<RocketLaunchInfo>) => {
             this.launchRocketsData = data;
             const nextLaunch: RocketLaunchInfo = data[0];
-            // mymap.setView(nextLaunch.coordinates, 13);
-            this.options['center'] = latLng(nextLaunch.coordinates);
-            data.forEach((element) => {
-                let texto = '<b>' + element.name + '</b><br />'
-                    + element.id + '</b><br />'
-                    + 'Hora: ' + element.date.toLocaleTimeString() + '<br />'
-                    + 'Fecha: ' + element.date.toLocaleDateString() + '<br />'
-                    + 'Cohete: ' + element.rocket;
-
-                if (element.url != null) {
-                    texto = texto + '<br />'
-                        + 'Url: ' + element.url;
-                }
-
-                const actualMarker = marker(element.coordinates, {
-                    icon: icon({
-                        iconSize: [25, 41],
-                        iconAnchor: [13, 41],
-                        iconUrl: 'assets/marker-icon.png',
-                        shadowUrl: 'assets/marker-shadow.png'
-                    }),
-                    title: element.name
-                });
-
-                this.options.layers.push(actualMarker);
-                this.layersControl.overlays[element.id.toString()] = actualMarker;
-
-            });
+            this.center = latLng(nextLaunch.coordinates);
+            data.forEach((element: RocketLaunchInfo) => this.addMarker(element.name, element.coordinates));
         });
 
 
         this.dataSource.onSelectedLaunchChangeListener((id: Number) => {
-            const selected = this.launchRocketsData.find((element: RocketLaunchInfo) => {
-                return element.id === id;
-            });
-
-            // mymap.setView(selected.coordinates, 13);
-            this.options['center'] = latLng(selected.coordinates);
+            const selected = this.launchRocketsData.find((element: RocketLaunchInfo) => element.id === id);
+            this.center = latLng(selected.coordinates);
         });
     }
 }
